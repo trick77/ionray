dea89fa7c40fd4d2047b1ff4aebc2d054047e85f
trick77 <jan@trick77.com>
2018-07-29 12:35:57 +0200

    BNA aktualisiert.

--- fabric/deploy.sh
+++ fabric/deploy.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
+#!/bin/bash -e
 #
 # Deployt initial das BNA (Business Network Archive) in Hyperledger Fabric.
 #
+npm run prepublish
 composer network install -a dist/ionray.bna -c PeerAdmin@hlfv1
 composer network start -n ionray-network -c PeerAdmin@hlfv1 -V 1.0.0 -A admin -S adminpw
 composer card import -f admin@ionray-network.card
