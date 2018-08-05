497de1a8140ecae6752f6c10208323dbc45b5f0b
trick77 <jan@trick77.com>
2018-08-05 09:28:40 +0200

    Skripte optimiert.

--- composer/start-api-server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# Stellt unter port tcp/3000 ein REST API für das ionray-network zur Verfügung,
-# womit die Clients dieses Business Netzwerks interagieren können.
-#
-
-screen -dm composer-rest-server -c admin@ionray-network -n always -w true &
-
-
--- composer/start-playground-server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-# Startet den Composer Playground unter tcp/8080.
-#
-
-screen -d -m composer-playground
-
-
